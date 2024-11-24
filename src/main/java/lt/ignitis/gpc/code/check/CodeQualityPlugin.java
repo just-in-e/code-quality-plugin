bdd6a3164babf2ef944fdd3951ea91ac70e9830b
Justyna <justyna.zachazevska@ignitis.lt>
2024-11-24 14:49:27 +0200

    Add checkstyle plugin

--- src/main/java/lt/ignitis/gpc/code/check/CodeQualityPlugin.java
+++ src/main/java/lt/ignitis/gpc/code/check/CodeQualityPlugin.java
@@ -2,15 +2,13 @@ package lt.ignitis.gpc.code.check;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.file.FileCollection;
+import org.gradle.api.plugins.quality.CheckstyleExtension;
+import org.gradle.api.plugins.quality.CheckstylePlugin;
 import org.gradle.api.plugins.quality.CodeNarcExtension;
 import org.gradle.api.plugins.quality.CodeNarcPlugin;
 import org.gradle.api.plugins.quality.PmdExtension;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static lt.ignitis.gpc.code.check.utility.RulesetFileUtility.copyRulesetToTemp;
@@ -25,6 +23,10 @@ public class CodeQualityPlugin implements Plugin<Project> {
 //        project.getPlugins().apply(PmdPlugin.class);
 //        configurePmd(project);
 
+        // Apply Checkstyle plugin
+        project.getPlugins().apply(CheckstylePlugin.class);
+        configureCheckstyle(project);
+
         //Apply CodeNarc plugin
         project.getPlugins().apply(CodeNarcPlugin.class);
         configureCodeNarc(project);
@@ -32,31 +34,35 @@ public class CodeQualityPlugin implements Plugin<Project> {
     }
 
     private void configurePmd(Project project) {
-        // Configure PMD
         project.getExtensions().configure(PmdExtension.class, pmd -> {
             pmd.setToolVersion("6.55.0"); // PMD version
-            pmd.setRuleSetFiles(project.files("config/pmd/pmd-ruleset.xml")); // PMD ruleset
             pmd.setIgnoreFailures(false); // Fail the build on violations
-        });
 
-        // Ensure the PMD ruleset file is available
-        project.afterEvaluate(proj -> {
-            if (!proj.file("config/pmd/pmd-ruleset.xml").exists()) {
-                proj.copy(copySpec -> {
-                    copySpec.from(getClass().getResourceAsStream("/pmd-ruleset.xml"));
-                    copySpec.into(proj.file("config/pmd"));
-                });
-            }
+            // Load ruleset from resources
+            Path rulesetPath = copyRulesetToTemp(project, "pmd-ruleset.xml");
+            FileCollection ruleSetFiles = project.files(rulesetPath.toFile());
+            pmd.setRuleSetFiles(ruleSetFiles);
+
+        });
+    }
+
+    private void configureCheckstyle(Project project) {
+        project.getExtensions().configure(CheckstyleExtension.class, checkstyle -> {
+            checkstyle.setToolVersion("10.20.1"); // Checkstyle version
+
+            // Load ruleset from resources
+            Path rulesetPath = copyRulesetToTemp(project, "checkstyle-ruleset.xml");
+            checkstyle.setConfigFile(rulesetPath.toFile());
         });
     }
 
     private void configureCodeNarc(Project project) {
-        // Configure CodeNarc
         project.getExtensions().configure(CodeNarcExtension.class, codeNarc -> {
             codeNarc.setToolVersion("3.3.0"); // CodeNarc version
             codeNarc.setReportFormat("console");
             codeNarc.setMaxPriority1Violations(0); // Fail on priority 1 violations
             codeNarc.setMaxPriority2Violations(0); // Fail on priority 2 violations
+            codeNarc.setMaxPriority3Violations(500); // Fail on priority 3 violations
 
             // Load the remote ruleset
 //            Path rulesetPath = downloadRemoteRuleset(project, "https://raw.githubusercontent.com/just-in-e/code-quality-rules/refs/heads/main/codenarc-ruleset.xml", "codenarc-ruleset.xml");
